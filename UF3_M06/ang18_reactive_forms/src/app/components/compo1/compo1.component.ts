dec9a5210bcac4a8e8a499bb41fad979955d686a
Pauk <56691890+pauca881@users.noreply.github.com>
2025-01-04 22:58:30 +0100

    Add files via upload

---
+++ UF3_M06/ang18_reactive_forms/src/app/components/compo1/compo1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-compo1',
+  standalone: true,
+  imports: [],
+  templateUrl: './compo1.component.html',
+  styleUrl: './compo1.component.css'
+})
+export class Compo1Component {
+
+}
