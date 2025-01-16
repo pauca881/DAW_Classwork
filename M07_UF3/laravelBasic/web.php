440d87e9b1088979d93ac537291d012a22a0b426
Pauk <56691890+pauca881@users.noreply.github.com>
2025-01-16 14:55:45 +0100

    Update web.php

--- M07_UF3/laravelBasic/web.php
+++ M07_UF3/laravelBasic/web.php
@@ -1,27 +1,31 @@
-<?php
+    <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\mycontroller;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('inici');
-    });
-    
-Route::get('/holamundo', function () {
-    return view('app');
-    });
-    
-Route::get('/holatodos', function () {
-    return 'hola a toos';
-    });
+    Route::get('/', function () {
+        return view('inici');
+        });
+        
+    Route::get('/holamundo', function () {
+        return view('app');
+        });
+        
+    Route::get('/holatodos', function () {
+        return 'hola a toos';
+        });
 
-Route::get('/index', function () {
-    return view('index');
-    });
+    Route::get('/index', function () {
+        return view('index');
+        });
 
-Route::get('/nosaltres', function () {
-    return view('nosaltres');
-    })->name('nosaltres');
-    
-Route::get('/onestem', function () {
-    return view('onestem');
-    })->name('onestem');
+    Route::get('/nosaltres', function () {
+        return view('nosaltres');
+        })->name('nosaltres');
+        
+    Route::get('/onestem', function () {
+        return view('onestem');
+        })->name('onestem');
+
+    Route::get('/insertar', [mycontroller::class, 'f_formulari'])->name('dades_insertar');
+    Route::post('/insertar', [mycontroller::class, 'f_insert'])->name('dades_insertar');
