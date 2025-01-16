a8a0a7a7cdfc920f21fbd6a6fd7873e5bc976403
Pauk <56691890+pauca881@users.noreply.github.com>
2025-01-16 14:54:35 +0100

    Insertar blade php

---
+++ M07_UF3/laravelBasic/insertar.blade.php
@@ -0,0 +1,25 @@
+@extends('app')
+
+@section('content')
+
+
+<div class="mb-3">
+    
+    <form action="{{ route('dades_insertar') }}" method="POST">
+        @csrf
+        @if (session('success'))
+            <h6 class="alert alert-success">{{ session('success') }}</h6>
+        @endif
+        @error('name')
+            <h5 class="alert alert-danger">{{ $message }}</h5>
+        @enderror
+
+        <label for="" class="form-label">Nombre</label>
+        <input type="text" class="form-control" id="name" name="name">
+        <button type="submit" class="btn btn-primary">Insertar</button>
+    </form>
+
+
+</div>
+
+@endsection
